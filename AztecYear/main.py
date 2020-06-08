8670c047c326331f3be883945afc5a50041c3a12
Phillip Manalili-Simeon <manalili18@up.edu>
2020-06-08 11:11:24 -0700

    first optimization, 109 bytes

--- AztecYear/main.py
+++ AztecYear/main.py
@@ -1,4 +1,4 @@
 import sys
 i=int(sys.argv[1])
-s=['Tochtli','Acatl','Tecpatl','Calli']
-print(str((i+2)%13+1)+' '+str(s[(i+2)%4]))
+s=['Tecpatl','Calli','Tochtli','Acatl']
+print(str((i+2)%13+1)+' '+str(s[i%4]))
