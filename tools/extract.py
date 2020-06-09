b089a4d3e0c300c539c323e92bd73156187b0d78
Phillip Manalili-Simeon <manalili18@up.edu>
2020-06-09 07:59:57 -0700

    fixed date ambiguity on refs; started prototype for file io

--- tools/extract.py
+++ tools/extract.py
@@ -6,17 +6,19 @@ def main():
     init()
     print(prompt)
     # TODO make files/folders, extract the prompt
+    create_working_dir()
 
 def init():
     init_argparse()
     init_beautiful_soup()
 
+# TODO take in just a title name and search through the page for the closest one
 def init_argparse():
     global name, url
 
     # Description: How to setup/use argparse
     # Source: https://docs.python.org/3/library/argparse.html
-    # Date: 08-06-2020
+    # Date Accessed: 2020-06-08
     parser = argparse.ArgumentParser(description='Setup new codegolfing env')
     parser.add_argument('name', help='Challenge title')
     parser.add_argument('url', help='URL of challenge')
@@ -30,11 +32,10 @@ def init_beautiful_soup():
 
     # Description: How to readout html from a URL
     # Source: https://stackoverflow.com/questions/24153519/how-to-read-html-from-a-url-in-python-3
-    # Date Accessed: 08-06-2020
-    fp = urllib.request.urlopen("https://codegolf.stackexchange.com/questions/205791/fizz-buzz-ave-a-banana")
+    # Date Accessed: 2020-06-08
+    fp = urllib.request.urlopen(url)
     mybytes = fp.read()
 
-    # TODO setup the soup
     soup = bs4.BeautifulSoup(mybytes,features="html.parser")
 
     #print(soup.findAll("div",{"class":"post-text"}).prettify())
@@ -42,8 +43,13 @@ def init_beautiful_soup():
     prompt += '\n\n\n'
     prompt += soup.find("div",{"class":"post-text"}).prettify()
 
+def create_working_dir():
 
+    # Description: File i/o help
+    # Source: https://www.tutorialspoint.com/python/python_files_io.htm
+    # Date Accessed: 2020-06-09
 
+    return
 
 if __name__ == "__main__":
     main()
